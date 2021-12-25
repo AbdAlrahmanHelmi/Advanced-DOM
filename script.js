130a9c49e47a22bb4b100b7fe425d6bda97c9d8f
Abd Alrahman Helmi <abdhelmi2018@gmail.com>
2021-12-25 14:21:06 +0200

    od2

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ document.querySelector(`.nav__links`).addEventListener(`click`, function (e) {
   console.log(e.target);
 
   if (e.target.classList.contains(`nav__link`)) {
-    const id =e.target.getAttribute(`href`);
+    const id = e.target.getAttribute(`href`);
     console.log(id);
     document.querySelector(id).scrollIntoView({ behavior: `smooth` });
   }
